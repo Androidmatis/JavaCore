642bd9971bc6d1ef317dea466b5c09b9b66a6bcc
andrey <androidmatis@gmail.com>
2024-11-13 22:30:07 +0600

    Add Chapter 15 (up to 454 pages)

---
+++ Shildt/15/LambdaDemo3.java
@@ -0,0 +1,15 @@
+// Демонстрация использования лямбда-выражения, принимающего два параметра.
+interface NumericTest2 {
+    boolean test(int n, int d);
+}
+class LambdaDemo3 {
+    public static void main(String[] args) {
+        // Это лямбда-выражение выясняет, является ли одно число
+        // множителем другого.
+        NumericTest2 isFactor = (n, d) -> (n % d) == 0;
+        if (isFactor.test(10, 2))
+            System.out.println("2 является множителем 10");
+        if (!isFactor.test(10, 3))
+            System.out.println("3 не является множителем 10");
+    }
+}
